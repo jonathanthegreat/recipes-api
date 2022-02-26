fc9006284e69d06aa6d405bc388b5b7af790c889
Cyberoxi Devops <cyberoxi.devops@gmail.com>
2022-02-26 18:38:26 +0330

    new recipe endpoint

--- main.go
+++ main.go
@@ -2,20 +2,43 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
 	"time"
 )
 
-func main() {
-	router := gin.Default()
-
-	router.Run()
-}
-
-// Recipe New data model
 type Recipe struct {
+	ID 				string		`json:"id"`
 	Name        	string   	`json:"name"`
 	Tags        	[]string 	`json:"tags"`
 	Ingredients 	[]string 	`json:"ingredients"`
 	Instructions	[]string	`json:"instructions"`
 	PublishedAt		time.Time	`json:"published_at"`
 }
+
+func newRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+
+func main() {
+	router := gin.Default()
+	router.POST("/recipes", newRecipeHandler)
+	router.Run()
+}
+
+var recipes []Recipe
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
